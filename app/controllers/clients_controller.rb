0aa39654ad43b765d4fe3d23734744808758f9ea
Nattida Wong <nattidac.wong@gmail.com>
2021-01-03 14:52:33 -0500

    added get request for new clients and created new erb form

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,29 +1,30 @@
 class ClientsController < ApplicationController
 
+    #new - loads a form
+    get '/clients/new' do
+        erb :"/clients/new"
+    end
+
+    #create - process the form and creates a user
+
+    #index - loads all clients
     get '/clients' do
         @clients = Client.all
 
         erb :"/clients/index"
     end
 
+    #show - get details on an indiviual user
     get '/clients/:id' do
         @client = Client.find_by(id: params[:id])
 
         erb :"/clients/show"
     end
 
-end
-
-    #new - loads a form
-
-    #create - process the form and creates a user
-
-    #index - loads all coaches
-
-    #show - get details on an indiviual user
-
     #edit - loading a form to edit a client
 
     #update
 
     #delete
+
+end
