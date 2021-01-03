14e4d1895756d68bbfc07838e72f634a5592ac4c
Nattida Wong <nattidac.wong@gmail.com>
2021-01-03 00:41:48 -0500

    created post signup git push '

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
     #Signup
+    #Load form
     get '/signup' do
         erb :"users/signup"
     end
 
-    #Load form
-
     #Process form and create user
+    post '/signup' do
         ##Create user and log them in
+        user = User.new(params[:user])
+        if user.save
+            session[:user_id] = user.id
+            redirect "/clients"
+        else
+            @errors = user.errors.full_messages.join(" - ")
+            erb :"users/signup"
+        end
+    end
 
     #Login
 
