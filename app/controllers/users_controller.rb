32757afe3ac2d6fb3156702d910d87fb78c8e6fd
Nattida Wong <nattidac.wong@gmail.com>
2021-01-03 00:33:39 -0500

    mount users controller in config.ru and add signup.erb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+    #Signup
+    get '/signup' do
+        erb :"users/signup"
+    end
+
+    #Load form
+
+    #Process form and create user
+        ##Create user and log them in
+
+    #Login
+
+    #Logout
+    
+end
