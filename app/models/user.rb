5d4253373215e3fab6dacbe2ccf873140bcbbde0
Nattida Wong <nattidac.wong@gmail.com>
2021-01-03 19:44:18 -0500

    added users validations for password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
     has_secure_password
     validates :username, presence: true
     validates :username, uniqueness: true
-
+    validates :password, length: { in: 8..20 }
+    
     has_many :clients
 end
